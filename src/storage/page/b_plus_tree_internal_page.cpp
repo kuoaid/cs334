97d433bac704cd23617333a08f66d26c3398ac4b
kuoaid <awang8396@gmail.com>
2021-05-27 00:19:04 -0500

    pull new. keep current changes. 2

--- src/storage/page/b_plus_tree_internal_page.cpp
+++ src/storage/page/b_plus_tree_internal_page.cpp
@@ -25,7 +25,6 @@ namespace bustub {
  * max page size.
  */
 INDEX_TEMPLATE_ARGUMENTS
-<<<<<<< HEAD
 void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {
   SetPageType(IndexPageType::INTERNAL_PAGE);
   SetSize(0);
@@ -33,9 +32,6 @@ void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id
   SetParentPageId(parent_id);
   SetMaxSize(max_size);
 }
-=======
-void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {}
->>>>>>> 3f4e886bac77b68a823533807f66cca0ed71e3da
 
 /*
  * Get the key stored at index.
@@ -43,13 +39,8 @@ void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id
 INDEX_TEMPLATE_ARGUMENTS
 KeyType B_PLUS_TREE_INTERNAL_PAGE_TYPE::KeyAt(int index) const {
   // replace with your own code (fill in the curly braces with the appropriate key value)
-<<<<<<< HEAD
   assert(index >= 0 && index < GetSize());
   return array[index].first;
-=======
-  KeyType key{};
-  return key;
->>>>>>> 3f4e886bac77b68a823533807f66cca0ed71e3da
 }
 
 /*
